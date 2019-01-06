6661fb16070ebb2bab016b0746f486eaf6f24fb2
DESKTOP-7JHHMQS\david <wi17b068@technikum-wien.at>
2019-01-06 13:04:07 +0100

    Example5 added

---
+++ Examples/Example5/Gui/ViewModel/DataVm.cs
@@ -0,0 +1,190 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TcpCom;
+
+namespace Gui.ViewModel
+{
+    public class DataVm : ViewModelBase
+    {
+        private ObservableCollection<ItemVm> buttonList = new ObservableCollection<ItemVm>();
+
+        public ObservableCollection<ItemVm> ButtonList
+        {
+            get { return buttonList; }
+            set { buttonList = value; RaisePropertyChanged(); }
+        }
+
+        private RelayCommand<ItemVm> toogleCmd;
+
+        public RelayCommand<ItemVm> ToogleCmd
+        {
+            get { return toogleCmd; }
+            set { toogleCmd = value; RaisePropertyChanged(); }
+        }
+
+        public ServerClient myServerClient { get; set; }
+
+        private RelayCommand listenCmd;
+
+        public RelayCommand ListenCmd
+        {
+            get { return listenCmd; }
+            set { listenCmd = value; RaisePropertyChanged(); }
+        }
+
+        private RelayCommand connectCmd;
+
+        public RelayCommand ConnectCmd
+        {
+            get { return connectCmd; }
+            set { connectCmd = value; RaisePropertyChanged(); }
+        }
+
+        private bool isOn = false;
+
+        private bool isServer = false;
+
+        private char[] seperator = new char[] { ('*') };
+
+        private Messenger myMessenger = SimpleIoc.Default.GetInstance<Messenger>();
+
+
+        public DataVm()
+        {
+            
+            FillButtonList();
+            ToogleCmd = new RelayCommand<ItemVm>(ToogleExec,ToogleCanExec);
+            ListenCmd = new RelayCommand(ListenExec, ListenCanExec);
+            ConnectCmd = new RelayCommand(ConnectExec, ConnectCanExec);
+            
+        }
+
+        private bool ConnectCanExec()
+        {
+            if (isOn) return false;
+            return true;
+        }
+
+        private void ConnectExec()
+        {
+            isServer = false;
+            myServerClient = new ServerClient(false, "127.0.0.1", 10100, GuiUpdater);
+            isOn = true;
+        }
+
+        private bool ListenCanExec()
+        {
+            if (isOn) return false;
+            return true;
+        }
+
+        private void ListenExec()
+        {
+            isServer = true;
+            myServerClient = new ServerClient(isServer, "127.0.0.1", 10100, new Action<string, ClientHandler>(GuiUpdater));
+            isOn = true;
+        }
+
+        private bool ToogleCanExec(ItemVm arg)
+        {
+            if (isOn) return true;
+            return false;
+        }
+
+        private void ToogleExec(ItemVm obj)
+        {
+            if (obj.IsOn)
+            {
+                SendDeactivateMessage(obj);
+            }
+            else
+            {
+                SendActivateMessage(obj);
+            }
+
+            MessageBox.Show(obj.IsOn + "" + obj.Num);
+            
+        }
+
+        private void SendActivateMessage(ItemVm obj)
+        {
+            if (isServer)
+            {
+                myServerClient.sendToAllClients(obj.Num + "*" + "on");
+            }
+            else { myServerClient.SendToServer(obj.Num + "*" + "on"); }
+            obj.IsOn = true;
+            DateTime t = new DateTime();
+            t = DateTime.Now;
+            myMessenger.Send<string>(obj.Num + "*on*" + t + "*Sended");
+        }
+
+        private void SendDeactivateMessage(ItemVm obj)
+        {
+            if (isServer)
+            {
+                myServerClient.sendToAllClients(obj.Num + "*" + "off");
+            }
+            else { myServerClient.SendToServer(obj.Num + "*" + "off"); }
+            obj.IsOn = false;
+            DateTime t = new DateTime();
+            t = DateTime.Now;
+            myMessenger.Send<string>(obj.Num + "*"+"off" +"*"+ t + "*Sended");
+        }
+
+        private void FillButtonList()
+        {
+            ButtonList.Add(new ItemVm());
+            ButtonList.Add(new ItemVm());
+            ButtonList.Add(new ItemVm());
+            ButtonList.Add(new ItemVm());
+        }
+
+        private void GuiUpdater(string message, ClientHandler client)
+        {
+            App.Current.Dispatcher.Invoke(() =>
+            {
+
+                string[] tmp = message.Split(seperator);
+                string number = tmp[0];
+                string switchTo = tmp[1];
+                int num = int.Parse(number);
+                bool status;
+                if (switchTo.Equals("on"))
+                {
+                    status = true;
+                }
+                else
+                {
+                    status = false;
+                }
+                foreach (var item in ButtonList)
+                {
+                    if (item.Num == num)
+                    {
+                        item.IsOn = status;
+                    break;
+                    }
+                }
+
+                DateTime t = new DateTime();
+                t = DateTime.Now;
+                myMessenger.Send<string>(num +"*"+ switchTo + "*"+ t  + "*Received");
+
+            });
+            
+
+        }
+
+
+    }
+}
---
+++ Examples/Example5/TcpCom/ClientHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TcpCom
+{
+    public class ClientHandler
+    {
+        private Socket socket;
+        private Action<string, ClientHandler> guiUpdater;
+        private byte[] buffer = new byte[1024];
+
+        public ClientHandler(Socket socket, Action<string, ClientHandler> guiUpdater)
+        {
+            this.socket = socket;
+            this.guiUpdater = guiUpdater;
+            Thread receiveThread = new Thread(new ThreadStart(Receiving));
+            receiveThread.IsBackground = true;
+            receiveThread.Start();
+        }
+
+        private void Receiving()
+        {
+            while (true)
+            {
+                int length=socket.Receive(buffer);
+                string tmp = Encoding.ASCII.GetString(buffer, 0, length);
+                guiUpdater(tmp, this);
+            }
+        }
+
+        public void send(string msg)
+        {
+            this.socket.Send(Encoding.ASCII.GetBytes(msg));
+        }
+
+    }
+}
